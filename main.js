4191cfd6ff504a7daae4409fe0b722ebdcf48d82
jadj4266-png <jadj4266@gmail.com>
2025-12-02 19:33:50 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // استهداف العناصر
+  const answer = document.querySelector('.answer');
+  const countdown = document.getElementById('countdown');
+  const countText = document.getElementById('count-text');
 
+  // اخفاء الإجابة في البداية
+  answer.style.display = 'none';
+
+  // بدء العد
+  let seconds = 3;
+  countText.textContent = seconds; // عرض الرقم الأول مباشرة
+
+  const interval = setInterval(() => {
+    seconds--;
+    if (seconds > 0) {
+      countText.textContent = seconds;
+    } else {
+      clearInterval(interval);       // إيقاف العد
+      countdown.style.display = 'none'; // اخفاء العداد
+      answer.style.display = 'block';   // اظهار الإجابة
+    }
+  }, 1000);
+});
